d665564e6c435f582d91dedf577af1ab958a3f34
BigPordy <Markus_kwh@web.de>
2026-05-31 12:38:31 +0200

    Create login.js file in api directory

---
+++ api/login.js
@@ -0,0 +1,13 @@
+# Im Repository-Verzeichnis:
+
+# Ordner erstellen
+mkdir pages api lib public
+
+# Datei-Struktur
+touch vercel.json
+touch .env.local
+touch api/occupancy.js
+touch api/login.js
+touch pages/index.html
+touch lib/database.js
+touch lib/api-client.js
